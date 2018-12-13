f8de96f7e37475d2cfd61fb5ba78149ab9f73625
zfv200 <zach.vary@flatironschool.com>
2018-12-13 14:25:18 -0500

    minor navbar change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,7 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink
-        to="/"
-      >Home</NavLink>
+      <NavLink to="/">Home</NavLink>
     </div>
   );
 };
