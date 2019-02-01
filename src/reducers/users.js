08a2f7098d5fcbf72d176a1f6b41088ffd5edbc4
zfv200 <zach.vary@flatironschool.com>
2019-02-01 17:07:50 -0500

    built out register auth

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,14 @@
 const defaultState = {
-  currentUser: null
+  currentUser: null,
+  signInError: false
 }
 
 export default function userReducer(state=defaultState, action){
   switch(action.type){
     case "SET_CURRENT_GHOST":
-      return {...state, currentUser: action.payload}
+      return {...state, currentUser: action.payload, signInError: false}
+    case "SIGN_IN_ERROR":
+      return {...state, signInError: true}
     default:
       return state
   }
