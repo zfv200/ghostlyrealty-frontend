f33ba0fff9ff9c6c70fc219c6c3108b9f1abbe3b
zfv200 <zach.vary@flatironschool.com>
2019-03-28 17:51:23 -0400

    css is updating well

--- src/HouseSearchForm/HouseSearchForm.css.js
+++ src/HouseSearchForm/HouseSearchForm.css.js
@@ -1,6 +1,9 @@
 export default {
-  typedSearch: {
-    height: '40px',
-    width: '800px'
+  // typedSearch: {
+  //   height: '40px',
+  //   width: '800px'
+  // }
+  form: {
+    maxHeight: "87px",
   }
 }
